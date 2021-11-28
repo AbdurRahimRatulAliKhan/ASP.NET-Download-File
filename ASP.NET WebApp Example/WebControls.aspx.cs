872749dac4eb9db1a211cc1af38dbb9e458bfa1a
Ratul Ali <rahim498617@gmail.com>
2021-11-29 00:02:19 +0600

    project uploaded

---
+++ ASP.NET WebApp Example/WebControls.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP.NET_WebApp_Example
+{
+    public partial class WebControls : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string filePath = "C:\Users\Ratul Ali\Desktop\New folder\About.aspx";
+            FileInfo file = new FileInfo(filePath);
+
+            if (file.Exists)
+            {
+                Response.Clear();
+                Response.AddHeader("Content-Disposition", "attachment; filename=" + file.Name);
+                Response.AddHeader("Content-Length", file.Length.ToString());
+                Response.ContentType = "text/plain";
+                Response.Flush();
+                Response.TransmitFile(file.FullName);
+                Response.End();
+            }
+            else Label1.Text = "Requested file is not available to download";
+        }
+    }
+}
